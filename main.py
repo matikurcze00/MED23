204763b2353f422507e05c3a7f7835674ce60415
matikurcze00 <pysiak2000@gmail.com>
2024-01-18 09:02:33 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from eclat import eclat, generate_rules_multithread
+from eclat import eclat
 import argparse
 
 from utils import load_dataset, save_file
@@ -9,21 +9,18 @@ def run(args):
     data = load_dataset(args.dataset)
     min_support = args.min_support
     vertical_database = eclat(data, args.processes_num, min_support)
-    print("rules")
-    min_confidence = args.min_confidence
-    association_rules = generate_rules_multithread(vertical_database,args.processes_num,min_confidence)
+   
     
     #Save file
-    save_file(association_rules)
+    save_file(vertical_database)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', default="test_dataset_2.dat", type=str, help="Dataset name")
-    parser.add_argument('--processes_num', default=2, type=int, help="Number of processors used in algorithm")
+    parser.add_argument('--dataset', default="T10I4D100K.dat", type=str, help="Dataset name")
+    parser.add_argument('--processes_num', default=6, type=int, help="Number of processors used in algorithm")
     parser.add_argument('--min_support', default=500, type=int, help="Minimal support")
-    parser.add_argument('--min_confidence', default=0.7, type=float, help="Minimal confidence")
 
     run(parser.parse_args(args))
     
